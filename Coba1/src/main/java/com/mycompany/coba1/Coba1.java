d39b9513774f24648ca756fa908ad8a8331b6691
RahmaLZahra <145528893+RahmaLZahra@users.noreply.github.com>
2024-09-11 08:12:22 +0700

    Add files via upload

---
+++ Coba1/src/main/java/com/mycompany/coba1/Coba1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.coba1;
+
+/**
+ *
+ * @author HP
+ */
+public class Coba1 {
+
+    public static void main(String[] args) {
+        // Menampilkan informasi siswa
+        System.out.println("NIS     : 05550033");
+        System.out.println("Nama    : Muhammad Andhy");
+        System.out.println("Alamat  : Ngawi");
+        System.out.println("Gender  : L");
+        System.out.println("Kelas   : A");
+    }
+}
---
+++ PrintpPattern/src/main/java/com/mycompany/printppattern/PrintpPattern.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.printppattern;
+
+/**
+ *
+ * @author HP
+ */
+public class PrintpPattern {
+
+    public static void main(String[] args) {
+        // Pola bagian kiri
+        System.out.print("  *\n");
+        System.out.print(" ***\n");
+        System.out.print("******\n");
+        System.out.print("*******\n");
+
+        // Spasi antara dua pola
+        System.out.print("       ");
+
+        // Pola bagian kanan
+        System.out.print("*\n");
+        System.out.print(" ***********\n");
+        System.out.print("  *********\n");
+        System.out.print("   *******\n");
+        System.out.print("    *****\n");
+        System.out.print("     ***\n");
+        System.out.print("      *\n");
+    }
+}
